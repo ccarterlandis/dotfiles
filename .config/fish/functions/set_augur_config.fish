0bf3dea72607f779720154a858cf43953e3c3880
Carter Landis <c@carterlandis.com>
2020-03-08 14:23:35 -0500

    Update g_au to add new -s flag
    Signed-off-by: Carter Landis <c@carterlandis.com>

--- .config/fish/functions/set_augur_config.fish
+++ .config/fish/functions/set_augur_config.fish
@@ -1,5 +1,10 @@
 function set_augur_config
     set config_home $HOME'/Projects/Work/configs'
-    export AUGUR_CONFIG_FILE=$config_home/$argv.config.json
+    set config_file $config_home/$argv.config.json
+    if test -f $config_file
+        export AUGUR_CONFIG_FILE=$config_file
+    else
+        echo $config_file does not exist
+    end
 end
 
