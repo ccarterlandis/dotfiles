d8c124d9b814490435d512b41d5a56f1992d4868
Carter Landis <c@carterlandis.com>
2020-03-18 12:07:06 -0500

    Fix fish prompt
    Signed-off-by: Carter Landis <c@carterlandis.com>

--- .config/fish/conf.d/pure.fish
+++ .config/fish/conf.d/pure.fish
@@ -1,42 +1,49 @@
-set --universal pure_version 2.1.8 # used for bug report
+set --global pure_version 2.5.2 # used for bug report
 
 # Base colors
-_pure_set_default pure_color_primary (set_color blue)
-_pure_set_default pure_color_info (set_color cyan)
-_pure_set_default pure_color_mute (set_color brblack)
-_pure_set_default pure_color_success (set_color magenta)
-_pure_set_default pure_color_normal (set_color normal)
-_pure_set_default pure_color_danger (set_color red)
-_pure_set_default pure_color_light (set_color white)
-_pure_set_default pure_color_warning (set_color yellow)
-_pure_set_default pure_color_dark (set_color black)
+_pure_set_default pure_color_primary blue
+_pure_set_default pure_color_info brblack
+_pure_set_default pure_color_mute white
+_pure_set_default pure_color_success green
+_pure_set_default pure_color_normal normal
+_pure_set_default pure_color_danger red
+_pure_set_default pure_color_light white
+_pure_set_default pure_color_warning yellow
+_pure_set_default pure_color_dark black
 
 # Prompt
 _pure_set_default pure_symbol_prompt "❯"
 _pure_set_default pure_symbol_reverse_prompt "❮"  # used for VI mode
-_pure_set_default pure_color_prompt_on_error $pure_color_danger
-_pure_set_default pure_color_prompt_on_success $pure_color_success
+_pure_set_default pure_color_prompt_on_error pure_color_danger
+_pure_set_default pure_color_prompt_on_success pure_color_success
 
 # Current Working Directory
-_pure_set_default pure_color_current_directory $pure_color_primary
+_pure_set_default pure_color_current_directory pure_color_primary
 
 # Git
-_pure_set_default pure_symbol_git_unpulled_commits "⇣"
-_pure_set_default pure_symbol_git_unpushed_commits "⇡"
-_pure_set_default pure_symbol_git_dirty "*"
-_pure_set_default pure_color_git_unpulled_commits $pure_color_info
-_pure_set_default pure_color_git_unpushed_commits $pure_color_info
-_pure_set_default pure_color_git_branch $pure_color_mute
-_pure_set_default pure_color_git_dirty $pure_color_mute
+_pure_set_default pure_enable_git true
+_pure_set_default pure_symbol_git_unpulled_commits "ahead"
+_pure_set_default pure_symbol_git_unpushed_commits "behind"
+_pure_set_default pure_symbol_git_dirty "dirty"
+_pure_set_default pure_symbol_git_clean "clean"
+_pure_set_default pure_color_git_unpulled_commits pure_color_warning
+_pure_set_default pure_color_git_unpushed_commits magenta
+_pure_set_default pure_color_git_branch cyan
+_pure_set_default pure_color_git_dirty pure_color_danger
+_pure_set_default pure_color_git_clean pure_color_success
 
 # SSH info
-_pure_set_default pure_color_ssh_hostname $pure_color_mute
-_pure_set_default pure_color_ssh_separator $pure_color_mute
-_pure_set_default pure_color_ssh_user_normal $pure_color_mute
-_pure_set_default pure_color_ssh_user_root $pure_color_light
+_pure_set_default pure_color_ssh_hostname pure_color_mute
+_pure_set_default pure_color_ssh_separator pure_color_mute
+_pure_set_default pure_color_ssh_user_normal pure_color_mute
+_pure_set_default pure_color_ssh_user_root pure_color_light
 
-# Virtualenv for Pyhon
-_pure_set_default pure_color_virtualenv $pure_color_mute
+# Number of running jobs
+_pure_set_default pure_show_jobs false
+_pure_set_default pure_color_jobs pure_color_normal
+
+# Virtualenv for Python
+_pure_set_default pure_color_virtualenv yellow
 
 # Print current working directory at the beginning of prompt
 # true (default):   current directory, git, user@hostname (ssh-only), command duration
@@ -49,12 +56,8 @@ _pure_set_default pure_begin_prompt_with_current_directory true
 _pure_set_default pure_separate_prompt_on_error false
 
 # Max execution time of a process before its run time is shown when it exits
-_pure_set_default pure_threshold_command_duration 5
-_pure_set_default pure_color_command_duration $pure_color_warning
-
-# Right Prompt variables
-_pure_set_default pure_right_prompt ""
-_pure_set_default pure_color_right_prompt $pure_color_normal
+_pure_set_default pure_threshold_command_duration 2
+_pure_set_default pure_color_command_duration pure_color_normal
 
 # VI mode indicator
 # true (default):  indicate a non-insert mode by reversing the prompt symbol (❮)
--- .config/fish/functions/_pure_prompt_separator.fish
+++
@@ -1,3 +0,0 @@
-function _pure_prompt_separator
-    echo (set_color white)"on"
-end
