cb01e56b0ace30cebe7350df013ac8104f031e66
Carter Landis <c@carterlandis.com>
2020-03-18 19:32:10 -0500

    Remove pure from version control
    Signed-off-by: Carter Landis <c@carterlandis.com>

--- .config/fish/functions/_pure_prompt_virtualenv.fish
+++
@@ -1,8 +0,0 @@
-function _pure_prompt_virtualenv --description "Display virtualenv directory"
-    if test -n "$VIRTUAL_ENV"
-        set --local virtualenv (basename "$VIRTUAL_ENV")
-        set --local virtualenv_color (_pure_set_color $pure_color_virtualenv)
-
-        echo (set_color white)"(""$virtualenv_color$virtualenv"(set_color white)")"
-    end
-end
