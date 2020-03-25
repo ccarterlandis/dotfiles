144a912d9fc6f16e80393050b99ad97ac9ae0c4a
Carter Landis <c@carterlandis.com>
2020-03-25 12:42:27 -0500

    Update host-specific configurations
    Signed-off-by: Carter Landis <c@carterlandis.com>

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -8,15 +8,17 @@ export DOCKER_BUILDKIT=1
 # Prompt
 export pure_symbol_prompt="∫"
 
-# Because macOS doesn't like Python multithreading
-export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 
+if test (hostname) = "kaiyote"
+    export JAVA_HOME=(/usr/libexec/java_home)
+    # Because macOS doesn't like Python multithreading
+    export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+end
+
+if test (hostname) = "beastmode"
+    set PATH $PATH /usr/local/go/bin
+end
 
 if which go > /dev/null 2>&1
     set PATH $PATH (go env GOBIN)
-end
-
-# Java
-if test (uname) = "Darwin"
-    export JAVA_HOME=(/usr/libexec/java_home)
 end
