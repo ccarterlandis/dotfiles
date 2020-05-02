a036b43e0fdf9adce3af3c24bc68f2503c41640f
Carter Landis <c@carterlandis.com>
2020-05-02 18:33:18 -0500

    Add reflash_keyboard and augur_go

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -8,7 +8,6 @@ export DOCKER_BUILDKIT=1
 # Prompt
 export pure_symbol_prompt="∫"
 
-
 if test (hostname) = "laputa"
     export JAVA_HOME=(/usr/libexec/java_home)
     # Because macOS doesn't like Python multithreading
---
+++ .config/fish/functions/reflash_keyboard.fish
@@ -0,0 +1,4 @@
+# Defined in /var/folders/jz/bsr_rmn930z9d6fb_ngc_64h0000gn/T//fish.85eBFN/reflash_keyboard.fish @ line 1
+function reflash_keyboard
+	wally-cli $HOME/.config/layouts/spacefiish.hex
+end
